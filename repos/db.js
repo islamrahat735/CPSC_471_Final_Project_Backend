fdfc2af5676077b673b06646b437d75c7e04a995
Rahat <islamrahat735@gmail.com>
2021-12-16 00:57:47 -0700

    added aws connection

--- repos/db.js
+++ repos/db.js
@@ -8,4 +8,12 @@ var connection = mysql.createConnection({
   database: 'DAYCARE'
 });
 
+//USE THIS CONNECTION INSTEAD IF YOU WANT TO CONNECT TO THE CLOUD DATABASE
+// var connection = mysql.createConnection({
+//   host: 'daycare1.cuhvrmx7alrb.us-east-2.rds.amazonaws.com',
+//   user: 'admin',
+//   password: 'daycarecpsc471',
+//   database: 'DAYCARE'
+// }) ;
+
 module.exports = connection;
